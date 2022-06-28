b380f453b611f92279a8c10a61b3e1a703046516
Shashi Chilukuri <ck.shashi@gmail.com>
2022-06-28 19:00:48 -0400

    Adding comments to train.py file

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+# Importing required libraries
 import os
 import joblib
 import argparse
@@ -16,7 +17,6 @@ from azureml.data.dataset_factory import TabularDatasetFactory
 data_path = path = "https://raw.githubusercontent.com/ShashiChilukuri/Data2Deployment-AzureML/main/heart_failure_clinical_records_dataset.csv"
 data = TabularDatasetFactory.from_delimited_files(data_path)
 
-#Saving model for current iteration
 run = Run.get_context()
 
 # Cleaning and splitting features and label data
@@ -26,19 +26,19 @@ y_df = x_df.pop("DEATH_EVENT")
 # Splitting data into train and test sets
 x_train, x_test, y_train, y_test = train_test_split(x_df, y_df, test_size=0.3, random_state=42)
 
+# Transforming the data using the standard Scaler
 scaler = StandardScaler()
 x_train = scaler.fit_transform(x_train)
 x_test = scaler.transform(x_test) 
 
 def main():
-    # Adding arguments to script
+    # Adding arguments
     parser = argparse.ArgumentParser()
-
     parser.add_argument('--n_estimators', type=int, default=20, help="N.O trees in the forest")
     parser.add_argument('--min_samples_split', type=int, default=2, help="Min samples to split")
-    
     args = parser.parse_args(args=[])
     
+    # adding log messages
     run.log("N.O trees in the forest:", np.int(args.n_estimators))
     run.log("Min samples to split:", np.int(args.min_samples_split))
     
@@ -46,12 +46,12 @@ def main():
                                    random_state=22, 
                                    min_samples_split=args.min_samples_split).fit(x_train, y_train)
 
+    # Calculating accuracy and adding to log messages
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", float(accuracy))
 
-    # Saving model for current iteration
+    # Creating directory and saving the model
     os.makedirs('outputs', exist_ok=True)
-    # joblib.dump(model, 'outputs/hyperDrive_{}_{}'.format(args.n_estimators, args.min_samples_split))
     joblib.dump(value=model, filename='outputs/model.pkl')
 
 if __name__ == '__main__':
